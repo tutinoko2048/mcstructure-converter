d44acf3ca361d5f3b66d443f565e3c34868934f2
Retoruto9900K <tutinoko2048@gmail.com>
2023-02-08 12:55:57 +0000

    Material UI!!!

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '../styles/globals.css'
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    // TODO:テーマ設定を行います
+});
+
+export default theme;
